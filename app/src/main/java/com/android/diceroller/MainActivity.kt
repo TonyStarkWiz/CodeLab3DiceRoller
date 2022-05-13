8b1def4fa6a9e81dcae3bdc1a1c2df121d9eab7f
TonyStarkWiz <tony_stark@knights.ucf.edu>
2022-05-13 13:24:36 -0400

    unit testing

--- app/src/main/java/com/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/android/diceroller/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import android.widget.Toast
 import android.view.View
 import android.Manifest
+import android.widget.ImageView
 import java.io.IOException
 
 /**
@@ -21,17 +22,31 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        rollDice()
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+         val drawableResource = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 
     // Create new Dice object with 6 sides and roll it
@@ -42,5 +57,4 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
 }
